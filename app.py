e95057bc7bd2c7a1d51aaa0d47377ace4a434433
b_kris <bryankristofer25@gmail.com>
2023-04-16 20:56:44 +0800

    Initial upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import webview
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(os.getcwd(), 'test.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(50))
+
+db.create_all()
+window = webview.create_window('myapp', app)
+
+@app.route('/')
+def index():
+    tests = Test.query.all()
+    return render_template('index.html', tests=tests)
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    data = request.json['data']
+    test = Test(data=data)
+    db.session.add(test)
+    db.session.commit()
+    return jsonify({'message': 'Record added successfully!'})
+
+if __name__ == '__main__':
+    webview.start(http_server=True, http_port=5000)
+
+# run `pyinstaller myapp.spec` to build
